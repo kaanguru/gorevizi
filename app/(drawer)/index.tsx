85eb0bd598ea3b4a8fdc38f0799de4acc12b98ac
kaanguru <kaanguru@mail.ru>
2024-12-11 09:33:51 +0300

    fab button

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -3,7 +3,9 @@ import { Stack } from 'expo-router';
 import { Container } from '~/components/Container';
 import { View, TextInput, TouchableOpacity, Text } from 'react-native';
 import { useRouter } from 'expo-router';
-
+import { Box } from '~/components/ui/box';
+import { Fab, FabLabel, FabIcon } from '@/components/ui/fab';
+import { AddIcon } from '~/components/ui/icon';
 export default function TaskList() {
   const router = useRouter();
 
@@ -12,16 +14,18 @@ export default function TaskList() {
       <Stack.Screen options={{ title: 'Tasks' }} />
       <Container>
         <Text>Tasks</Text>
-        <TouchableOpacity
-          onPress={() => router.push('/(tasks)/create-task')}
-          style={{
-            backgroundColor: '#007AFF',
-            padding: 10,
-            borderRadius: 8,
-            marginTop: 10,
-          }}>
-          <Text style={{ color: 'white', textAlign: 'center' }}>Add Task</Text>
-        </TouchableOpacity>
+        <Box className="absolute bottom-5 right-5 bg-background-50">
+          <Fab
+            size="md"
+            onPress={() => router.push('/(tasks)/create-task')}
+            placement="bottom right"
+            isHovered={false}
+            isDisabled={false}
+            isPressed={false}>
+            <FabIcon as={AddIcon} />
+            <FabLabel>Add Task</FabLabel>
+          </Fab>
+        </Box>
       </Container>
     </>
   );
