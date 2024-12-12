9935faeec96d7f09d5d7ad66e0b4da25295b8f19
kaanguru <kaanguru@mail.ru>
2024-12-12 17:08:23 +0300

    chore: update 1 file and delete 2 files

--- supabase/migrations/20241212053548_period_frequency_change.sql
+++
@@ -1,9 +0,0 @@
-alter table "public"."tasks" drop column "repeat_fre";
-
-alter table "public"."tasks" drop column "repeat_perd";
-
-alter table "public"."tasks" add column "repeat_frequency" smallint;
-
-alter table "public"."tasks" add column "repeat_period" repeat_period;
-
-
