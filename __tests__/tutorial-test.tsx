b5da2f67d1fb9e7dfcd3392d9a573b08a369b633
kaanguru <kaanguru@mail.ru>
2024-12-17 13:21:42 +0300

    feat ✨: added new pattern to .gitignore, unit tests for tutorialscreen and updated eas.json configurations
    - Added a new pattern to the .gitignore file to ignore coverage data.
    - Added unit test for TutorialScreen component with two test cases: test renders correctly and test navigates through tutorials.
    - Updated configurations for CLI and production environments in eas.json file.
    - Removed the "build:local" script and added several new scripts for testing and Jest configuration.
    - Imported new dependencies and updated package resolutions, specifically adding support for React Native testing library.

---
+++ __tests__/tutorial-test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import TutorialScreen from '../app/(onboarding)/tutorial';
+import { useRouter } from 'expo-router';
+
+// Mock the useRouter hook
+jest.mock('expo-router', () => ({
+  useRouter: jest.fn(),
+}));
+describe('TutorialScreen', () => {
+  // Setup mock router before each test
+  const mockReplace = jest.fn();
+  beforeEach(() => {
+    (useRouter as jest.Mock).mockReturnValue({ replace: mockReplace });
+  });
+
+  // Clear mocks after each test
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders correctly', () => {
+    const { getByText } = render(<TutorialScreen />);
+    setTimeout(() => {
+      expect(getByText('Tutorial 1')).toBeTruthy();
+    }, 2300);
+  });
+
+  it('navigates through tutorials', () => {
+    const { getByText } = render(<TutorialScreen />);
+
+    const continueButton = getByText('Continue');
+
+    fireEvent.press(continueButton);
+    expect(getByText('Tutorial 2')).toBeTruthy();
+
+    fireEvent.press(continueButton);
+    setTimeout(() => {
+      expect(getByText('Tutorial 3')).toBeTruthy();
+    }, 1300);
+    const getStartedButton = getByText('Get Started');
+    fireEvent.press(getStartedButton);
+
+    // Verify navigation to start screen
+    expect(mockReplace).toHaveBeenCalledWith('/(onboarding)/start');
+  });
+});
