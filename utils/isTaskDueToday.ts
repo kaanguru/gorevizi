286dda53b0194da1176f1330f93ac88d819abd7d
kaanguru <kaanguru@mail.ru>
2025-01-18 17:42:42 +0300

    isTaskDueToday

--- utils/isTaskDueToday.ts
+++ utils/isTaskDueToday.ts
@@ -1,23 +1,36 @@
 import { Tables } from '~/database.types';
 import { DayOfWeek } from '~/types';
 import getCurrentDayOfWeek from '~/utils/getCurrentDayOfWeek';
+import { addDays, addWeeks, addMonths, addYears, isSameDay } from 'date-fns';
 
-export default function isTaskDueToday(task: Readonly<Tables<'tasks'>>): boolean {
-  return task.repeat_on_wk ? true : false;
-  /* if (!task.repeat_on_wk && !task.repeat_frequency && !task.repeat_period) {
-    return true; // Task has no due date
+function isTaskDueToday(task: Readonly<Tables<'tasks'>>): boolean {
+  const createdAtDate = new Date(task.created_at);
+  const today = new Date();
+
+  switch (task.repeat_period) {
+    case 'Daily':
+      return isSameDay(addDays(createdAtDate, task.repeat_frequency ?? 0), today);
+
+    case 'Weekly':
+      if (!task.repeat_on_wk) return false;
+      const currentDayOfWeek = getCurrentDayOfWeek();
+      return (
+        task.repeat_on_wk.includes(currentDayOfWeek as unknown as DayOfWeek) &&
+        isSameDay(addWeeks(createdAtDate, task.repeat_frequency ?? 0), today)
+      );
+
+    case 'Monthly':
+      return isSameDay(addMonths(createdAtDate, task.repeat_frequency ?? 1), today);
+
+    case 'Yearly':
+      return isSameDay(addYears(createdAtDate, task.repeat_frequency ?? 1), today);
+
+    case null:
+      return true;
+
+    default:
+      return false;
   }
-
-  if (task.repeat_period === 'Daily') {
-    return true; // TODO: Add logic to check if the task is due today based on the start date and repeat frequency
-  }
-
-  if (task.repeat_period === 'Weekly' && task.repeat_on_wk) {
-    const today = getCurrentDayOfWeek;
-    return task.repeat_on_wk.includes(today as unknown as DayOfWeek);
-  }
-
-  // Add more logic for Monthly and Yearly tasks if needed
-
-  return false; */
 }
+
+export default isTaskDueToday;
