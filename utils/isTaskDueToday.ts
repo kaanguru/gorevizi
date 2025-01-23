eb47e644bbc10521079ab6981d769b84137914db
kaanguru <kaanguru@mail.ru>
2025-01-23 18:03:53 +0300

    function isTaskDueToday complated

--- utils/isTaskDueToday.ts
+++ utils/isTaskDueToday.ts
@@ -1,33 +1,46 @@
 import { Tables } from '~/database.types';
 import { DayOfWeek } from '~/types';
 import getCurrentDayOfWeek from '~/utils/getCurrentDayOfWeek';
-import { addWeeks, addMonths, addYears, isSameDay, differenceInDays } from 'date-fns';
+import {
+  addWeeks,
+  addMonths,
+  addYears,
+  isSameDay,
+  differenceInDays,
+  differenceInWeeks,
+  differenceInMonths,
+  differenceInYears,
+} from 'date-fns';
 
 const today = new Date();
 function isTaskDueToday(task: Readonly<Tables<'tasks'>>): boolean {
   const createdAtDate = new Date(task.created_at);
-
+  const repeatFrequency = task.repeat_frequency ?? 1;
+  const daysSinceCreation = differenceInDays(today, createdAtDate);
   switch (task.repeat_period) {
     case 'Daily':
-      return isDailyTaskDueToday(createdAtDate, task.repeat_frequency);
+      return isDailyTaskDueToday(daysSinceCreation, task.repeat_frequency);
 
     case 'Weekly':
       if (!task.repeat_on_wk) return false;
       const currentDayOfWeek = getCurrentDayOfWeek();
       if (!task.repeat_on_wk.includes(currentDayOfWeek as unknown as DayOfWeek)) return false;
+      return differenceInWeeks(today, createdAtDate) % repeatFrequency === 0;
 
-      const daysSinceCreation = differenceInDays(today, createdAtDate);
-      const weeksSinceCreation = Math.floor(daysSinceCreation / 7);
-      const repeatFrequency = task.repeat_frequency ?? 1;
-
-      return daysSinceCreation >= 0 && weeksSinceCreation % repeatFrequency === 0;
-
-    case 'Monthly':
-      return isSameDay(addMonths(createdAtDate, task.repeat_frequency ?? 1), today);
-
-    case 'Yearly':
-      return isSameDay(addYears(createdAtDate, task.repeat_frequency ?? 1), today);
-
+    case 'Monthly': {
+      const monthsDiff = differenceInMonths(today, createdAtDate);
+      if (monthsDiff <= 0) return false;
+      if (monthsDiff % repeatFrequency !== 0) return false;
+      const expectedDate = addMonths(createdAtDate, monthsDiff);
+      console.log('🚀 ~ file: isTaskDueToday.ts:35 ~ expectedDate:', expectedDate);
+      return isSameDay(expectedDate, today);
+    }
+    case 'Yearly': {
+      const yearsDiff = differenceInYears(today, createdAtDate);
+      if (yearsDiff < 0) return false;
+      const expectedDate = addYears(createdAtDate, yearsDiff);
+      return isSameDay(expectedDate, today);
+    }
     case null:
       return true;
 
@@ -37,8 +50,7 @@ function isTaskDueToday(task: Readonly<Tables<'tasks'>>): boolean {
   }
 }
 
-function isDailyTaskDueToday(createdAtDate: Date, repeat_frequency: number | null): boolean {
-  const daysSinceCreation = differenceInDays(today, createdAtDate);
+function isDailyTaskDueToday(daysSinceCreation: number, repeat_frequency: number | null): boolean {
   const repeatFrequency = repeat_frequency ?? 1;
   return daysSinceCreation >= 0 && daysSinceCreation % repeatFrequency === 0;
 }
