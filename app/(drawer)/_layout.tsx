4096d74a7690ff93e7c444cd3867cb44888d48de
kaanguru <kaanguru@mail.ru>
2025-01-27 18:57:12 +0300

    useCallback handle re-order fix

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -2,7 +2,15 @@ import { Ionicons } from '@expo/vector-icons';
 import { Drawer } from 'expo-router/drawer';
 import { router } from 'expo-router';
 import { useAuth } from '~/utils/auth';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      staleTime: 60_000, // Prevent over-fetching
+    },
+  },
+});
 const DrawerLayout = () => {
   const { signOut } = useAuth();
 
@@ -12,43 +20,45 @@ const DrawerLayout = () => {
   };
 
   return (
-    <Drawer screenOptions={{ headerShown: true }}>
-      <Drawer.Screen
-        name="index"
-        options={{
-          headerTitle: 'Due Tasks',
-          drawerLabel: 'Due Tasks',
-          drawerIcon: ({ size, color }) => (
-            <Ionicons name="list-outline" size={size} color={color} />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="(settings)/index"
-        options={{
-          headerTitle: 'Settings',
-          drawerLabel: 'Settings',
-          drawerIcon: ({ size, color }) => (
-            <Ionicons name="settings-outline" size={size} color={color} />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="signout"
-        options={{
-          headerTitle: 'Sign Out',
-          drawerLabel: 'Sign Out',
-          drawerIcon: ({ size, color }) => (
-            <Ionicons name="log-out-outline" size={size} color={color} />
-          ),
-        }}
-        listeners={{
-          drawerItemPress: () => {
-            handleSignOut();
-          },
-        }}
-      />
-    </Drawer>
+    <QueryClientProvider client={queryClient}>
+      <Drawer screenOptions={{ headerShown: true }}>
+        <Drawer.Screen
+          name="index"
+          options={{
+            headerTitle: 'Due Tasks',
+            drawerLabel: 'Due Tasks',
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="list-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="(settings)/index"
+          options={{
+            headerTitle: 'Settings',
+            drawerLabel: 'Settings',
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="settings-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="signout"
+          options={{
+            headerTitle: 'Sign Out',
+            drawerLabel: 'Sign Out',
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="log-out-outline" size={size} color={color} />
+            ),
+          }}
+          listeners={{
+            drawerItemPress: () => {
+              handleSignOut();
+            },
+          }}
+        />
+      </Drawer>
+    </QueryClientProvider>
   );
 };
 
--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { FlatList, GestureResponderEvent, Pressable } from 'react-native';
 import { Stack, useFocusEffect, useRouter } from 'expo-router';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { supabase } from '~/utils/supabase';
 import { Tables } from '~/database.types';
 import { Fab, FabLabel, FabIcon } from '@/components/ui/fab';
@@ -11,69 +12,134 @@ import { Spinner } from '~/components/ui/spinner';
 import { TaskItem } from '~/components/DraggableTaskItem';
 import reOrder from '~/utils/reOrder';
 import isTaskDueToday from '~/utils/isTaskDueToday';
-import { Task } from '~/types';
 
 export default function TaskList() {
   const router = useRouter();
-  const [tasks, setTasks] = useState<ReadonlyArray<Task>>([]);
-  const [filteredTasks, setFilteredTasks] = useState<ReadonlyArray<Task>>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isFiltered, setIsFiltered] = useState<boolean>(false);
+  const queryClient = useQueryClient();
+  const [isFiltered, setIsFiltered] = useState(false);
+  const fetchTasks = async () => {
+    const { data, error } = await supabase
+      .from('tasks')
+      .select('*')
+      .eq('is_complete', false)
+      .order('position', { ascending: true, nullsFirst: true });
 
-  const fetchDueTasks = useCallback(async () => {
-    setIsLoading(true);
-    try {
-      const { data, error } = await supabase
-        .from('tasks')
-        .select('*')
-        .eq('is_complete', false)
-        .order('position', { ascending: true, nullsFirst: true });
-
-      error ? console.error('Error fetching tasks:', error.message) : setTasks(data ?? []);
-    } catch (error) {
-      console.error('Error fetching tasks:', error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
-
-  const updateTaskPositions = async (tasks: ReadonlyArray<Tables<'tasks'>>) => {
-    try {
-      const updates = tasks.map((task, index) =>
+    if (error) throw new Error(error.message);
+    return data;
+  };
+  // Query for fetching tasks
+  const {
+    data: tasks = [],
+    isLoading,
+    isRefetching,
+    refetch,
+  } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: fetchTasks,
+  });
+  useEffect(() => {
+    console.log('Fetched tasks:', tasks.length);
+  }, [tasks]);
+  useEffect(() => {
+    console.log('Component re-rendered. Tasks:', tasks.length);
+  }, [tasks]);
+  // Mutation for updating task positions
+  const updateTaskPositionsMutation = useMutation({
+    mutationFn: async (reorderedTasks: Tables<'tasks'>[]) => {
+      const updates = reorderedTasks.map((task, index) =>
         supabase.from('tasks').update({ position: index }).eq('id', task.id)
       );
       await Promise.all(updates);
-    } catch (error) {
-      console.error('Error updating task positions:', error);
-    }
-  };
+    },
+    onMutate: async (reorderedTasks) => {
+      await queryClient.cancelQueries({ queryKey: ['tasks'] });
+      const previousTasks = queryClient.getQueryData<Tables<'tasks'>[]>(['tasks']);
+      queryClient.setQueryData(['tasks'], reorderedTasks);
+      return { previousTasks };
+    },
+    onError: (err, _, context) =>
+      context?.previousTasks && queryClient.setQueryData(['tasks'], context.previousTasks),
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['tasks'] });
+    },
+  });
 
-  const handleReorder = async (from: number, to: number) => {
-    const mutableTasks = [...tasks];
-    const reorderedTasks = reOrder(from, to, mutableTasks);
-    setTasks(reorderedTasks);
-    await updateTaskPositions(reorderedTasks);
-  };
+  // Mutation for toggling task completion
+  const toggleCompleteMutation = useMutation({
+    mutationFn: async (params: Readonly<{ taskId: number; isComplete: boolean }>) => {
+      const { error } = await supabase
+        .from('tasks')
+        .update({ is_complete: params.isComplete })
+        .eq('id', params.taskId);
 
-  const handleToggleComplete = async (taskId: number, isComplete: boolean): Promise<void> => {
-    try {
-      await supabase.from('tasks').update({ is_complete: isComplete }).eq('id', taskId);
-      await fetchDueTasks();
-    } catch (error) {
-      console.error('Error toggling task completion:', error);
-    }
-  };
+      if (error) throw new Error(error.message);
+    },
+    onMutate: async ({ taskId, isComplete }) => {
+      await queryClient.cancelQueries({ queryKey: ['tasks'] });
+      const previousTasks = queryClient.getQueryData<Tables<'tasks'>[]>(['tasks']);
+      const newTasks = previousTasks?.map((task) =>
+        task.id === taskId ? { ...task, is_complete: isComplete } : task
+      );
+      queryClient.setQueryData(['tasks'], newTasks);
+      return { previousTasks };
+    },
+    onError: (err, variables, context) => {
+      queryClient.setQueryData(['tasks'], context?.previousTasks);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['tasks'] });
+    },
+  });
 
-  const handleFilterTodayPress = (event: GestureResponderEvent): void => {
-    const tempTasks = [...tasks];
-    setFilteredTasks(tempTasks.filter(isTaskDueToday));
+  // Filtered tasks memoization
+  const filteredTasks = useMemo(
+    () => (isFiltered ? tasks.filter(isTaskDueToday) : tasks),
+    [isFiltered, tasks]
+  );
+
+  const handleReorder = useCallback(
+    (from: number, to: number) => {
+      const currentTasks = queryClient.getQueryData<Tables<'tasks'>[]>(['tasks']) || [];
+      const currentFilteredTasks = isFiltered ? currentTasks.filter(isTaskDueToday) : currentTasks;
+      const sourceArray = [...currentFilteredTasks];
+
+      if (currentTasks.length === 0 && currentFilteredTasks.length === 0) {
+        console.warn('filtered and tasks arrays are empty, cannot reorder');
+        return;
+      }
+
+      if (sourceArray.length === 0) {
+        console.error('No tasks to reorder');
+        return;
+      }
+
+      const reorderedTasks = reOrder(from, to, sourceArray);
+      updateTaskPositionsMutation.mutate(reorderedTasks, {
+        onSuccess: () => {
+          console.log('Task positions updated successfully.');
+        },
+        onError: (error) => {
+          console.error('Failed to update task positions:', error);
+        },
+      });
+    },
+    [isFiltered, queryClient, updateTaskPositionsMutation]
+  );
+
+  const handleFilterTodayPress = () => {
     setIsFiltered(!isFiltered);
   };
-
+  const handleTaskUpdate = async (updatedTask: Readonly<Tables<'tasks'>>) => {
+    try {
+      await supabase.from('tasks').update(updatedTask).eq('id', updatedTask.id).select().single();
+    } catch (error) {
+      console.error('Task update failed:', error);
+    }
+  };
   useFocusEffect(
     useCallback(() => {
-      fetchDueTasks();
-    }, [fetchDueTasks])
+      refetch();
+    }, [refetch])
   );
 
   const renderTaskItem = useCallback(
@@ -81,12 +147,14 @@ export default function TaskList() {
       <TaskItem
         task={item}
         index={index}
-        onTaskUpdate={fetchDueTasks}
         onReorder={handleReorder}
-        onToggleComplete={handleToggleComplete}
+        onTaskUpdate={handleTaskUpdate}
+        onToggleComplete={(taskId, isComplete) =>
+          toggleCompleteMutation.mutateAsync({ taskId, isComplete })
+        }
       />
     ),
-    [fetchDueTasks, handleReorder, handleToggleComplete]
+    [toggleCompleteMutation.mutate, handleReorder]
   );
 
   return (
@@ -96,7 +164,7 @@ export default function TaskList() {
           title: 'Tasks',
           headerRight: () => (
             <>
-              <Pressable onPress={fetchDueTasks} className="p-5">
+              <Pressable onPress={() => refetch()} className="p-5">
                 <Icon as={DownloadIcon} className="m-1 h-6 w-6 text-typography-100" />
               </Pressable>
 
@@ -111,7 +179,7 @@ export default function TaskList() {
         }}
       />
       <Container>
-        {isLoading ? (
+        {isLoading || isRefetching ? (
           <Box className="flex-1 items-center justify-center">
             <Spinner size="large" />
           </Box>
@@ -122,7 +190,7 @@ export default function TaskList() {
               padding: 16,
               paddingBottom: 80,
             }}
-            data={isFiltered ? filteredTasks : tasks}
+            data={filteredTasks}
             renderItem={renderTaskItem}
             keyExtractor={(item) => item.id.toString()}
           />
