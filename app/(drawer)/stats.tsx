cffa0ac44cf5173fad138afd55675b038d725940
kaanguru <kaanguru@mail.ru>
2025-02-18 14:28:15 +0300

    feat: Add happy and healthy lottie animations and files
    - Add happy lottie component
    - Add healthy lottie component
    - Add happy lottie file
    - Add healthy lottie file

--- app/(drawer)/stats.tsx
+++ app/(drawer)/stats.tsx
@@ -2,15 +2,28 @@ import React from 'react';
 import { View, FlatList } from 'react-native';
 import { Tables } from '@/database.types';
 import useTasksQuery from '~/hooks/useTasksQueries';
-
+import useHealthAndHappinessQuery from '~/hooks/useHealthAndHappinessQueries.ts';
 import TaskSuccessPercentage from '~/components/TaskSuccessPercentage';
 import { Box } from '~/components/ui/box';
 import { Text } from '~/components/ui/text';
 import { Spinner } from '~/components/ui/spinner';
-
+import { Card } from '~/components/ui/card';
+import { HStack } from '~/components/ui/hstack';
+import { Heading } from '~/components/ui/heading';
+import { VStack } from '~/components/ui/vstack';
+import { Divider } from '~/components/ui/divider';
+import LogoPortrait from '~/components/lotties/LogoPortrait';
+import Happy from '~/components/lotties/Happy';
+import Healthy from '~/components/lotties/Healthy';
+import { useUser } from '~/hooks/useUser';
 export default function Stats() {
   const { data = [], isLoading, error } = useTasksQuery('completed');
-
+  const { data: user } = useUser();
+  const {
+    data: healthAndHappiness,
+    isLoading: isLoadingHealthAndHappiness,
+    error: errorHealthAndHappiness,
+  } = useHealthAndHappinessQuery(user?.id);
   if (isLoading) {
     return (
       <Box className="flex-1 items-center justify-center">
@@ -32,6 +45,36 @@ export default function Stats() {
 
   return (
     <View className="flex-1 justify-evenly bg-background-light dark:bg-background-dark">
+      <HStack className="basis-6/6 justify-evenly ">
+        <Card className=" m-1 w-2/6  rounded-lg bg-[#1982C4] p-5">
+          <Healthy height={120} width={120} />
+          <Heading className=" justify-between text-center text-typography-white dark:text-typography-black">
+            Health
+          </Heading>
+          <Divider
+            orientation="horizontal"
+            className="my-2 flex w-full self-center bg-background-500"
+          />
+
+          <Text bold size="5xl" className="p-3 text-center font-mono text-[#FFCA3A] ">
+            {healthAndHappiness?.health || 0}
+          </Text>
+        </Card>
+        <Card className=" m-1 w-2/6  rounded-lg bg-[#4F10A8] p-5">
+          <Happy height={120} width={120} />
+          <Heading className=" justify-between text-center text-typography-white dark:text-typography-black">
+            Happiness
+          </Heading>
+          <Divider
+            orientation="horizontal"
+            className="my-2 flex w-full self-center bg-background-500"
+          />
+
+          <Text bold size="5xl" className="p-3 text-center font-mono text-[#FFCA3A] ">
+            {healthAndHappiness?.happiness || 0}
+          </Text>
+        </Card>
+      </HStack>
       <FlatList
         contentContainerStyle={{
           gap: 8,
